89cc5da830a9fbf64446fcc2698d4635acae93a5
mohammad eunus <38430781+mohammadeunus@users.noreply.github.com>
2021-05-21 15:15:46 +0600

    aa

--- app/src/main/java/com/example/myapplicationempty/registration.java
+++ app/src/main/java/com/example/myapplicationempty/registration.java
@@ -4,40 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
-import java.lang.String;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
-
-public class registration<mobileNumber> extends AppCompatActivity {
-    public static final String TAG = "registration";/*
-    FirebaseAuth fAuth;
-    ProgressBar progressBar;
-    EditText fNidNumber = findViewById(R.id.NidNumber);
-    EditText fEditTextTextPersonName3 = findViewById(R.id.NidNumber);
-    EditText fEditTextPhone = findViewById(R.id.NidNumber);
-    EditText fEditTextTextEmailAddress = findViewById(R.id.NidNumber);
-    EditText fEditTextTextPassword = findViewById(R.id.NidNumber);
-    String MobileNumber = fEditTextPhone.getText().toString().trim();
-    String password = fEditTextTextPassword.getText().toString().trim();*/
-
+public class registration extends AppCompatActivity {
+    public static final String TAG="registration";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-/*
-        fAuth = FirebaseAuth.getInstance();
-        progressBar = findViewById(R.id.progressBar2);*/
 
     }
-
     @Override
     protected void onStart() {
         super.onStart();
@@ -70,34 +51,19 @@ public class registration<mobileNumber> extends AppCompatActivity {
 
     public void ans2reg(View view) {
         EditText editTextNumberForViewById = findViewById(R.id.editTextNumber);
-        String nidNumberForViewById = editTextNumberForViewById.getText().toString();
-        TextView textviewError2 = findViewById(R.id.textviewError2);
-        if (nidNumberForViewById.length() == 17) {
+        String nidNumberForViewById =editTextNumberForViewById.getText().toString();
+        TextView textviewError2 =findViewById(R.id.textviewError2);
+        if(nidNumberForViewById.length() == 7)
+        {
             textviewError2.setTextColor(Color.GREEN);
             textviewError2.setText("Registration Successful");
             Intent MainActivityScreen = new Intent(registration.this, MainActivity.class);
             startActivity(MainActivityScreen);
-        } else {
+        }
+        else
+        {
             textviewError2.setTextColor(Color.RED);
-            textviewError2.setText("Incorrect");
+            textviewError2.setText("incorrect");
         }
-
-        //user input error handling
-        /*
-        if(TextUtils.isEmpty(MobileNumber)){
-            fEditTextPhone.setError("mobile number Required.");
-            return;
-        }
-
-        if(TextUtils.isEmpty(password)){
-            fEditTextTextPassword.setError("Password is Required.");
-            return;
-        }
-
-        if(password.length() < 6){
-            fEditTextTextPassword.setError("Password Must be >= 6 Characters");
-        }*/
-
     }
-
 }
