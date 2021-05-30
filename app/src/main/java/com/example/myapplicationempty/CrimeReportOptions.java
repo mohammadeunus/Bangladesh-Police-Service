26f8e400aeb642f7a0ca4ed8413ec77c3c7cb2b6
jayed bin zakir <79739692+jayedbinzakir@users.noreply.github.com>
2021-05-30 20:13:04 +0600

    sd

--- app/src/main/java/com/example/myapplicationempty/CrimeReportOptions.java
+++ app/src/main/java/com/example/myapplicationempty/CrimeReportOptions.java
@@ -1,11 +1,20 @@
 package com.example.myapplicationempty;
-
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.content.ComponentName;
+import android.net.Uri;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CrimeReportOptions extends AppCompatActivity {
 
@@ -42,6 +51,22 @@ public class CrimeReportOptions extends AppCompatActivity {
         });
     }
 
+    public void gallery(View view) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        startActivity(intent);
+    }
+
+    public void Camera(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW , Uri.parse("android.media.action.IMAGE_CAPTURE"));
+        startActivity(intent);
+    }
+    public void Maps(View view) {
+        Uri uri = Uri.parse("geo://47.4925,19.0513");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
     public void profile(View view) {
         Intent user_profiles = new Intent(CrimeReportOptions.this, user_profile.class);
         startActivity(user_profiles);
